4f3baac1692855373424bfb7222674433d2007cb
Lucky Mbithi <luckymbithi002@gmail.com>
2024-07-05 17:16:17 +0300

    add mobile header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -18,47 +18,61 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-blue-500 text-white shadow-md mb-30 fixed top-0 w-full z-50">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="text-2xl font-bold">
-          <Link to="/">File Manager<i class="fa-regular fa-file mx-2"></i></Link>
+    <>
+      {/* Larger screen header */}
+      <header className="hidden sm:block bg-blue-500 text-white shadow-md mb-30 fixed top-0 w-full z-50">
+        <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+          <div className="text-2xl font-bold">
+            <Link to="/">File Manager<i class="fa-regular fa-file mx-2"></i></Link>
+          </div>
+          <nav className="space-x-4 flex justify-between items-center">
+            <Link to="/" className="hover:underline">
+              <i class="fa-solid fa-house text-green-300"></i> Home
+            </Link>
+            {status ? (
+              <>
+                <Link to="/file-list" className="hover:underline">
+                  <i class="fa-solid fa-file text-green-300"></i> Files
+                </Link>
+                <Link to="/dashboard" className="hover:underline">
+                  <i class="fa-solid fa-chart-line text-green-300"></i> Dashboard
+                </Link>
+                <div className='font-bold hover:border-gray-200 duration-200 transition-all p-1 bg-green-300 text-black'>
+                  <span className="ml-4">{userData.name}</span>
+                  <Button onClick={handleLogout} className="ml-4 bg-red-500 mx-2 rounded-[2rem]">
+                    Logout
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <button className="rounded-[2rem] py-2 px-3 hover:text-white text-center hover:bg-blue-300 transition-all duration-200 hover:underline">
+                    Login
+                  </button>
+                </Link>
+                <Link to="/signup">
+                  <button className="rounded-[2rem] py-2 px-3 hover:text-black text-center hover:bg-green-500 transition-all duration-200 hover:underline">
+                    Signup
+                  </button>
+                </Link>
+              </>
+            )}
+          </nav>
         </div>
-        <nav className="space-x-4 flex justify-between items-center">
-          <Link to="/" className="hover:underline">
-            Home
+      </header>
+      {/* Mobile Header */}
+      <header className="sm:hidden bg-blue-500 text-white shadow-md mb-30 fixed top-0 w-full z-50 flex flex-row justify-between items-center h-[4rem] px-2">
+        <div className="">
+          <h1>File Manager<i class="fa-regular fa-file mx-2"></i></h1>
+        </div>
+        <div className="">
+          <Link>
+            <i class="fa-solid fa-ellipsis-vertical mx-3"></i>
           </Link>
-          <Link to="/about" className="hover:underline">
-            About
-          </Link>
-          {status ? (
-            <>
-              <Link to="/file-list" className="hover:underline">
-                Uploads
-              </Link>
-              <div className='font-bold hover:border-gray-200 duration-200 transition-all p-1 bg-green-300 text-black'>
-                <span className="ml-4">{userData.name}</span>
-                <Button onClick={handleLogout} className="ml-4 bg-red-500 mx-2 rounded-[2rem]">
-                  Logout
-                </Button>
-              </div>
-            </>
-          ) : (
-            <>
-              <Link to="/login">
-                <button className="rounded-[2rem] py-2 px-3 hover:text-white text-center hover:bg-blue-300 transition-all duration-200 hover:underline">
-                  Login
-                </button>
-              </Link>
-              <Link to="/signup">
-                <button className="rounded-[2rem] py-2 px-3 hover:text-black text-center hover:bg-green-500 transition-all duration-200 hover:underline">
-                  Signup
-                </button>
-              </Link>
-            </>
-          )}
-        </nav>
-      </div>
-    </header>
+        </div>
+      </header>
+    </>
   );
 };
 
